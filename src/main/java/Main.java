b0fea171a9dce0e1751efbb0c6fe6a645613e7b7
lotsonL09 <willimaca09ac@hotmail.com>
2025-10-13 01:51:02 -0500

    trackFiles method update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,22 +14,24 @@ public class Main {
 //            System.out.println("Ocurrio un error");
 //        }
 
-//        PDF pdf = new PDF(Settings.PDF_DIRECTORY,
-//                Settings.PROCESSED_PDF_DIRECTORY,
-//                Settings.TXT_DIRECTORY);
-//        try{
-//            pdf.trackFiles(1);
-//        } catch (Exception e){
-//            e.printStackTrace();
-//            System.out.println("Ocurrio un error a");
-//        }
-
-        try {
-            PDF.renderProject(1,"D:/Will/Emprendimiento/Proyectos/results/secret_proyect/carpetPrueba");
+        PDF pdf = new PDF(Settings.PDF_DIRECTORY,
+                Settings.PROCESSED_PDF_DIRECTORY,
+                Settings.TXT_DIRECTORY);
+        try{
+            pdf.trackFiles(1);
         } catch (Exception e){
             e.printStackTrace();
-            System.out.println("Ocurrio un error");
+            System.out.println("Ocurrio un error a");
         }
+
+
+
+//        try {
+//            PDF.renderProject(1,"D:/Will/Emprendimiento/Proyectos/results/secret_proyect/carpetPrueba");
+//        } catch (Exception e){
+//            e.printStackTrace();
+//            System.out.println("Ocurrio un error");
+//        }
 
 
     }
